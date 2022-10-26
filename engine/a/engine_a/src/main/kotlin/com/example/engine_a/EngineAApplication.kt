a9db2495c15d932bc986cc7d9e71c7cd338e9818
limdongjin <geniuslim27@gmail.com>
2022-10-26 21:09:30 +0900

    Etc: Spring Boot 초기 파일 생성 engine_a #17

---
+++ engine/a/engine_a/src/main/kotlin/com/example/engine_a/EngineAApplication.kt
@@ -0,0 +1,11 @@
+package com.example.engine_a
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EngineAApplication
+
+fun main(args: Array<String>) {
+    runApplication<EngineAApplication>(*args)
+}
