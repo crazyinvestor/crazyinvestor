7b5ef60fe22ce9e9c7121e81678a6b601ce1912c
김형준 <ninini6630@gmail.com>
2022-10-27 21:59:57 +0900

    Fix: DB 구조 변경으로 인한 Exchange Enum 클래스 이름 변경 대응 Close #35

--- engine/b/src/main/java/net/crazyinvestor/engine_b/enums/ExchangeName.java
+++ engine/b/src/main/java/net/crazyinvestor/engine_b/enums/ExchangeName.java
@@ -3,13 +3,13 @@ package net.crazyinvestor.engine_b.enums;
 import lombok.Getter;
 
 @Getter
-public enum Exchange {
+public enum ExchangeName {
     BITHUMB("bithumb"),
     ;
 
     private final String exchangeName;
 
-    Exchange(final String exchangeName) {
+    ExchangeName(final String exchangeName) {
         this.exchangeName = exchangeName;
     }
 }
