7df37137c430decc21c08bb68b7a74867217bcff
limdongjin <geniuslim27@gmail.com>
2022-10-31 11:28:26 +0900

    Test: Integration Test for KafkaProducer-KafkaConsumer-Spring 관계 간 Serialization, Database Access

---
+++ engine/a/engine_aaa/src/test/kotlin/net/crazyinvestor/engine_aaa/TickerConsumerKafkaCassandraIntegrationTest.kt
@@ -0,0 +1,77 @@
+package net.crazyinvestor.engine_aaa
+
+import com.datastax.oss.driver.api.core.CqlSessionBuilder
+import com.datastax.oss.driver.api.core.uuid.Uuids
+import net.crazyinvestor.engine_aaa.constants.NEW_TICKER_TOPIC
+import net.crazyinvestor.engine_aaa.persistence.Ticker
+import org.apache.kafka.clients.producer.Callback
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import org.junit.jupiter.api.*
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.cassandra.core.CassandraTemplate
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("dev")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+class TickerConsumerKafkaCassandraIntegrationTest {
+    lateinit var cassandraTemplate: CassandraTemplate
+    lateinit var producerFactory: ProducerFactory<String, Ticker>
+    lateinit var producer: Producer<String, Ticker>
+
+    @BeforeAll
+    fun beforeAll(): Unit {
+        producerFactory = DefaultKafkaProducerFactory<String, Ticker>(PRODUCER_CONFIGS)
+        cassandraTemplate = CassandraTemplate(CqlSessionBuilder().withKeyspace("djdb").build())
+    }
+
+    @BeforeEach
+    fun setUp() { producer = producerFactory.createProducer() }
+
+    @AfterEach
+    fun tearDown() { producer.close() }
+
+    @AfterAll
+    fun afterAll() { producerFactory.closeThreadBoundProducer() }
+
+    @Test
+    fun `if produce 100 data, must stored 100 data into cassandra`() {
+        val expected = 100L
+         val initialCount: Long = cassandraTemplate.count(Ticker::class.java)
+
+        val callback: Callback =
+            Callback { metadata, exception -> println(metadata to exception) }
+
+        repeat(expected.toInt()){
+            producer.send(ProducerRecord(NEW_TICKER_TOPIC, getRandomTicker()), callback).get()
+        }
+
+        println("sleep 7s")
+        Thread.sleep(7000)
+
+        val afterCount = cassandraTemplate
+            .count(Ticker::class.java)
+
+        Assertions.assertEquals(expected, afterCount - initialCount)
+
+        repeat(10) { println() }
+        println("success")
+    }
+
+    companion object {
+        val PRODUCER_CONFIGS = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:29092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java
+        )
+        fun getRandomTicker(): Ticker {
+            return Ticker(Uuids.timeBased(),"ticker-${(Math.random()*10000).toInt()}", (Math.random()*10000).toInt())
+        }
+    }
+}
---
+++ engine/a/engine_aaa/src/test/kotlin/net/crazyinvestor/engine_aaa/TickerKafkaIntegrationTest.kt
@@ -0,0 +1,71 @@
+package net.crazyinvestor.engine_aaa
+
+import com.datastax.oss.driver.api.core.uuid.Uuids
+import net.crazyinvestor.engine_aaa.constants.NEW_TICKER_TOPIC
+import net.crazyinvestor.engine_aaa.persistence.Ticker
+import org.apache.kafka.clients.producer.*
+import org.apache.kafka.common.serialization.StringSerializer
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("dev")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+class TickerKafkaIntegrationTest {
+    lateinit var producerFactory: ProducerFactory<String, Ticker>
+    lateinit var producer: Producer<String, Ticker>
+
+    @BeforeAll
+    fun beforeAll(): Unit { producerFactory = DefaultKafkaProducerFactory<String, Ticker>(PRODUCER_CONFIGS) }
+
+    @BeforeEach
+    fun setUp() { producer = producerFactory.createProducer() }
+
+    @AfterEach
+    fun tearDown() { producer.close() }
+
+    @AfterAll
+    fun afterAll() { producerFactory.closeThreadBoundProducer() }
+
+    @Test
+    fun `can send valid input`() {
+        val callback: Callback =
+            Callback { metadata, exception -> println(metadata to exception) }
+
+        val metadata = producer.send(ProducerRecord(NEW_TICKER_TOPIC, VALID_TICKER), callback)
+            .get()
+
+        println(metadata)
+    }
+
+    @Test
+    fun `can send more tickers`() {
+        val callback: Callback =
+            Callback { metadata, exception -> println(metadata to exception) }
+
+        repeat(100){
+            producer.send(ProducerRecord(NEW_TICKER_TOPIC, getRandomTicker()), callback).get()
+        }
+    }
+
+    companion object {
+        val VALID_TICKER = Ticker(Uuids.timeBased(), "ticker-test-123", 123)
+        val PRODUCER_CONFIGS = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:29092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java
+        )
+        fun getRandomTicker(): Ticker {
+            return Ticker(Uuids.timeBased(),"ticker-${(Math.random()*10000).toInt()}", (Math.random()*10000).toInt())
+        }
+    }
+}
