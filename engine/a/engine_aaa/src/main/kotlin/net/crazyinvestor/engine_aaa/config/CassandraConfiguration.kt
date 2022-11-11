c2bdda158afd3f6d8a0fe662589bf660a6c63e94
limdongjin <geniuslim27@gmail.com>
2022-11-11 21:47:31 +0900

    feature: downgrade to ReactiveCassandra -> Cassandra

--- engine/a/engine_aaa/src/main/kotlin/net/crazyinvestor/engine_aaa/config/CassandraConfiguration.kt
+++ engine/a/engine_aaa/src/main/kotlin/net/crazyinvestor/engine_aaa/config/CassandraConfiguration.kt
@@ -3,20 +3,20 @@ package net.crazyinvestor.engine_aaa.config
 import com.datastax.oss.driver.api.core.CqlSession
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.cassandra.core.ReactiveCassandraTemplate
-import org.springframework.data.cassandra.core.cql.session.DefaultBridgedReactiveSession
+import org.springframework.data.cassandra.core.CassandraOperations
+import org.springframework.data.cassandra.core.CassandraTemplate
 import java.net.InetSocketAddress
 
 @Configuration
 class CassandraConfiguration {
     @Bean
-    fun reactiveCassandraTemplate(): ReactiveCassandraTemplate {
+    fun cassandraTemplate(): CassandraOperations {
         val cqlSession: CqlSession = CqlSession.builder()
             .withKeyspace(DEFAULT_KEYSPACE)
             .addContactPoint(InetSocketAddress("localhost", 9042))
             .withLocalDatacenter("datacenter1")
             .build()
 
-        return ReactiveCassandraTemplate(DefaultBridgedReactiveSession(cqlSession))
+        return CassandraTemplate(cqlSession)
     }
 }
